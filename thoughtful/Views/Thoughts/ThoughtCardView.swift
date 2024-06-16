d38d74608b70dcce29dc145572d75f1de144065a
Nabil Ridhwan <nabridhwan@gmail.com>
2024-06-16 18:14:02 +0800

    fix: fix thought card view having extra space when there is not attributes

--- thoughtful/Views/Thoughts/ThoughtCardView.swift
+++ thoughtful/Views/Thoughts/ThoughtCardView.swift
@@ -24,9 +24,6 @@ struct ThoughtCardView: View {
     
     let thought: Thought;
     
-    var emotionExists: Bool {
-        thought.emotion != nil
-    }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 5){
@@ -37,30 +34,35 @@ struct ThoughtCardView: View {
             Text(LocalizedStringKey(thought.thought_response))
                 .font(.callout)
                 .lineLimit(2)
-
-            HStack{
+            
+            if(thought.locationExists || thought.musicExists || thought.emotionExists){
                 
-                if (thought.location != nil) {
-                    ThoughtCardAttrbuteView(icon: "location.fill", text: "Eunos")
-                }
                 
-                if (thought.music != nil) {
-                    ThoughtCardAttrbuteView(icon: "music.note", text: "The Backseat Lovers - Pool House")
+                HStack{
+                    
+                    if (thought.locationExists) {
+                        ThoughtCardAttrbuteView(icon: "location.fill", text: "Eunos")
+                    }
+                    
+                    if (thought.musicExists) {
+                        ThoughtCardAttrbuteView(icon: "music.note", text: "The Backseat Lovers - Pool House")
+                    }
+                    
+                    if (thought.emotionExists) {
+                        ThoughtCardAttrbuteView(icon: "smiley.fill", text: thought.emotion!.description.capitalized, backgroundColor: emotionColors[thought.emotion!], foregroundColor: .black.opacity(0.6), shadowColor: emotionColors[thought.emotion!])
+                    }
                 }
+                .padding(.vertical, 2)
+                .padding(.top, 8)
                 
-                if (emotionExists) {
-                    ThoughtCardAttrbuteView(icon: "smiley.fill", text: thought.emotion!.description.capitalized, backgroundColor: emotionColors[thought.emotion!], foregroundColor: .black.opacity(0.6), shadowColor: emotionColors[thought.emotion!])
-                }
             }
-                   .padding(.vertical, 2)
-                   .padding(.top, 8)
         }
         .multilineTextAlignment(.leading)
         .frame(maxWidth: .infinity, alignment: .leading)
         .foregroundColor(.white)
         .padding()
         .background{
-                RoundedRectangle(cornerRadius: 24)
+            RoundedRectangle(cornerRadius: 24)
                 .foregroundStyle(.card)
         }
         .overlay( /// apply a rounded border
@@ -72,5 +74,5 @@ struct ThoughtCardView: View {
 
 #Preview {
     ThoughtCardView(thought: Thought(thought_prompt: "What are three things that I am grateful for?", thought_response: "My Friends, **Nazrul** for _checking up on me_, The movie night on Discord. I am gonna write a longer thought just to see if the lines would concatenate beacuse to be honest. This can get very long!", date_created: Date.now, location: "Eunos", music: "The Backseat Lovers - Pool House", emotion: .neutral))
-
+    
 }
