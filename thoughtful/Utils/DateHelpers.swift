187491208da2d2aa65a0745844e5812d135fc4fd
Nabil Ridhwan <nabridhwan@gmail.com>
2024-06-17 16:48:03 +0800

    fix: fix horizontal calendar date display
    horizontal calendar date now displays the first day of the week at the most left, like a normal 7 day week

---
+++ thoughtful/Utils/DateHelpers.swift
@@ -0,0 +1,60 @@
+//
+//  DateHelpers.swift
+//  Thoughtful
+//
+//  Created by Nabil Ridhwan on 17/6/24.
+//
+
+import Foundation
+
+enum DateHelpers {
+    static let dateFormatter = DateFormatter()
+
+    static func getDayOfWeekFromDate(date: Date) -> String {
+        dateFormatter.dateFormat = "EEE"
+        let dayInWeek = dateFormatter.string(from: date)
+        return dayInWeek
+    }
+
+    static func getDayFromDate(date: Date) -> String {
+        dateFormatter.dateFormat = "d"
+        let dayInWeek = dateFormatter.string(from: date)
+        return dayInWeek
+    }
+
+    static func isSameDay(_ date1: Date, _ date2: Date) -> Bool {
+        let calendar = Calendar.current
+        return calendar.isDate(date1, inSameDayAs: date2)
+    }
+
+    static func findFirstDateOfTheWeek() -> Date {
+        let calendar = Calendar.current
+        let weekday = calendar.component(.weekday, from: Date.now)
+        let daysToSubtract = weekday - calendar.firstWeekday
+        return calendar.date(byAdding: .day, value: -daysToSubtract, to: Date.now)!
+    }
+
+    static func findLastDateOfTheWeek() -> Date {
+        let calendar = Calendar.current
+        let weekday = calendar.component(.weekday, from: Date.now)
+        let daysToAdd = (calendar.firstWeekday + 6 - weekday) % 7
+        return calendar.date(byAdding: .day, value: daysToAdd, to: Date.now)!
+    }
+
+    static func getDatesForWeek() -> [Date] {
+        let calendar = Calendar.current
+        let firstDateOfTheWeek = findFirstDateOfTheWeek()
+        let lastDateOfTheWeek = findLastDateOfTheWeek()
+
+        var dates: [Date] = Array(repeating: Date(), count: 7)
+
+        dates[0] = firstDateOfTheWeek
+        dates[6] = lastDateOfTheWeek
+
+        for i in 1 ..< 6 {
+            dates[i] = calendar.date(byAdding: .day, value: i, to: firstDateOfTheWeek)!
+        }
+
+        return dates
+    }
+}
--- thoughtful/Views/Calendar/HorizontalCalendarView.swift
+++ thoughtful/Views/Calendar/HorizontalCalendarView.swift
@@ -18,71 +18,56 @@ var dates: [Date] = [
     Calendar.current.date(byAdding: .day, value: 0, to: Date())!,
 ]
 
-let dateFormatter = DateFormatter()
-
-func getDayOfWeekFromDate(date: Date) -> String {
-    dateFormatter.dateFormat = "EEE"
-    let dayInWeek = dateFormatter.string(from: date)
-    return dayInWeek
-}
-
-func getDayFromDate(date: Date) -> String {
-    dateFormatter.dateFormat = "d"
-    let dayInWeek = dateFormatter.string(from: date)
-    return dayInWeek
-}
-
-func isToday(_ date1: Date, _ date2: Date) -> Bool {
-    return getDayFromDate(date: date1) == getDayFromDate(date: date2)
-}
-
 struct HorizontalCalendarView: View {
     @Binding var selectedDate: Date
-
     @Namespace var rectNs;
 
+    var weekDates: [Date] {
+        DateHelpers.getDatesForWeek()
+    }
+
     var body: some View {
-        ZStack {
-            HStack {
-                ForEach(dates, id: \.hashValue) {
-                    date in
-                    Button {
-                        withAnimation {
-                            selectedDate = date
+        HStack {
+            ForEach(weekDates, id: \.hashValue) {
+                date in
+                Button {
+                    withAnimation {
+                        selectedDate = date
+                    }
+                } label: {
+                    ZStack {
+                        if DateHelpers.isSameDay(selectedDate, date) {
+                            RoundedRectangle(cornerRadius: 24)
+                                .frame(width: 55, height: 74)
+                                .foregroundStyle(.cardAttribute)
+                                .matchedGeometryEffect(id: "Rect", in: rectNs)
                         }
-                    } label: {
-                        ZStack {
-                            if isToday(selectedDate, date) {
-                                RoundedRectangle(cornerRadius: 24)
-                                    .frame(width: 55, height: 74)
-                                    .foregroundStyle(.cardAttribute)
-                                    .matchedGeometryEffect(id: "Rect", in: rectNs)
-                            }
 
-                            //                            if isToday(date, Date()){
-                            //                                RoundedRectangle(cornerRadius: 24)
-                            //                                    .position(x:4, y:-25)
-                            //                                    .frame(width: 5, height: 5)
-                            //                                    .foregroundStyle(.white)
-                            //
-                            //                            }
+                        //                            if isToday(date, Date()){
+                        //                                RoundedRectangle(cornerRadius: 24)
+                        //                                    .position(x:4, y:-25)
+                        //                                    .frame(width: 5, height: 5)
+                        //                                    .foregroundStyle(.white)
+                        //
+                        //                            }
 
-                            VStack {
-                                Text("\(getDayOfWeekFromDate(date: date))")
+                        VStack {
+                            Text("\(DateHelpers.getDayOfWeekFromDate(date: date))")
 
-                                Text("\(getDayFromDate(date: date))")
-                                    .font(.title3)
-                                    .fontWeight(.bold)
+                            Text("\(DateHelpers.getDayFromDate(date: date))")
+                                .font(.title3)
+                                .fontWeight(.bold)
 
-                            }.frame(maxWidth: .infinity)
-                                .foregroundStyle(.primary.opacity(isToday(selectedDate, date) ? 1.0 : 0.5))
-                        }
-                    }.scaleEffect(
-                        isToday(selectedDate, date) ? 1.1 : 1.0
-                    )
-                }
+                        }.frame(maxWidth: .infinity)
+                            .foregroundStyle(.primary.opacity(DateHelpers.isSameDay(selectedDate, date) ? 1.0 : 0.5))
+                    }
+                }.scaleEffect(
+                    DateHelpers.isSameDay(selectedDate, date) ? 1.1 : 1.0
+                )
             }
-        }.frame(maxHeight: 50)
+        }
+
+        .frame(maxHeight: 50)
     }
 }
 
