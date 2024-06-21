57262ab3ad3a01a6c1f95f7d9307090f59441bee
Nabil Ridhwan <nabridhwan@gmail.com>
2024-06-21 16:02:03 +0800

    feat: add back and forward buttons and thoughts indicator for days in Horizontal Calendar

--- thoughtful/Views/Calendar/HorizontalCalendarView.swift
+++ thoughtful/Views/Calendar/HorizontalCalendarView.swift
@@ -5,6 +5,7 @@
 //  Created by Nabil Ridhwan on 13/6/24.
 //
 
+import SwiftData
 import SwiftUI
 
 // var sample_dates: [Date] {
@@ -16,6 +17,7 @@ import SwiftUI
 // }
 
 struct HorizontalCalendarView: View {
+    @Environment(\.modelContext) var context: ModelContext
     @Binding var selectedDate: Date
     @Namespace var rectNs;
 
@@ -25,20 +27,46 @@ struct HorizontalCalendarView: View {
 
     var body: some View {
         HStack {
+            Button {
+                guard let date = Calendar.autoupdatingCurrent.date(byAdding: .day, value: -7, to: selectedDate) else {
+                    return
+                }
+
+                withAnimation {
+                    selectedDate = date
+                }
+
+            } label: {
+                Label("Previous Week", systemImage: "chevron.left")
+            }
+            .foregroundStyle(.primary.opacity(0.5))
+            .labelStyle(.iconOnly)
+
             ForEach(weekDates, id: \.hashValue) {
                 date in
+
+                let hasThoughts = fetchNumberOfThoughtsForDate(for: date) > 0
+
                 Button {
                     withAnimation(.easeOut(duration: 0.2)) {
                         selectedDate = date
                     }
                 } label: {
-                    ZStack {
+                    ZStack(alignment: .center) {
                         if DateHelpers.isSameDay(selectedDate, date) {
                             RoundedRectangle(cornerRadius: 24)
-                                .frame(width: 52, height: 74)
+                                .frame(width: 52, height: 80)
                                 .foregroundStyle(.cardAttribute)
                                 .matchedGeometryEffect(id: "Rect", in: rectNs)
                         }
+
+                        if hasThoughts {
+                            Circle()
+                                .frame(maxWidth: 6)
+                                .offset(y: 31)
+                                .opacity(0.2)
+                        }
+
                         VStack {
                             Text("\(DateHelpers.getDayOfWeekFromDate(date: date))")
 
@@ -61,13 +89,46 @@ struct HorizontalCalendarView: View {
                     DateHelpers.isAfterDate2(date, Date.now)
                 )
             }
+
+            Button {
+                guard let date = Calendar.autoupdatingCurrent.date(byAdding: .day, value: 7, to: selectedDate) else {
+                    return
+                }
+
+                withAnimation {
+                    selectedDate = date
+                }
+
+            } label: {
+                Label("Next Week", systemImage: "chevron.right")
+            }
+            .foregroundStyle(.primary.opacity(0.5))
+            .labelStyle(.iconOnly)
         }
 
         .frame(maxHeight: 50)
     }
 }
 
+extension HorizontalCalendarView {
+    func fetchNumberOfThoughtsForDate(for date: Date) -> Int {
+        let fetchDescriptor = FetchDescriptor<Thought>(
+            predicate: Thought.predicate(searchDate: date),
+            sortBy: [
+                SortDescriptor(\.date_created, order: .reverse),
+            ]
+        )
+
+        do {
+            return try context.fetchCount(fetchDescriptor)
+        } catch {
+            return 0
+        }
+    }
+}
+
 #Preview {
     HorizontalCalendarView(selectedDate: .constant(Date.now))
         .previewLayout(.sizeThatFits)
+        .modelContainer(SampleData.shared.modelContainer)
 }
