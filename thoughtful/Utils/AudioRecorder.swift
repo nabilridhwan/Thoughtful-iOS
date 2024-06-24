feaa109de5fa82520f62bd6faa50d18a67e6d52e
Nabil Ridhwan <nabridhwan@gmail.com>
2024-06-24 21:45:06 +0800

    fix: fix failed to set audio session category

--- thoughtful/Utils/AudioRecorder.swift
+++ thoughtful/Utils/AudioRecorder.swift
@@ -95,8 +95,7 @@ class AudioRecorder: NSObject, ObservableObject {
         do {
             // Set the audio session category and mode.
             // https://stackoverflow.com/a/65139199
-            try audioSession.setCategory(.record, mode: .default, options: .defaultToSpeaker)
-            try audioSession.setMode(.videoRecording)
+            try audioSession.setCategory(.record, mode: .default)
         } catch {
             print("Failed to set the audio session configuration")
             fatalError("Failed to set the audio session configuration")
